684eb634d005d4c00c8bf98533a968e686e573cc
joseMarcus <jose.marcus@academico.ifpb.edu.br>
2022-02-16 21:30:28 -0300

    criacao de formulario

---
+++ Cafeteria/js/model/dataset.js
@@ -0,0 +1,32 @@
+const dataset = [
+  {
+    id: 1,
+    name: 'Cafés',
+    image: 'img/cafe.jpg',
+    descri: 'O café é uma bebida produzida a partir dos grãos torrados do fruto do cafeeiro. É servido tradicionalmente quente, mas também pode ser consumido gelado.',
+    preco: 'A partir de R$ 1,99',
+  },
+  {
+    id: 2,
+    name: 'Bebidas',
+    image: 'img/bebida.jpg',
+    descri: 'Bebida é um tipo de líquido, o qual é especificamente preparado para consumo humano.',
+    preco: 'A partir de R$ 2,99',
+  },
+  {
+    id: 3,
+    name: 'Doces',
+    image: 'img/doce.jpg',
+    descri: 'Os doces são conservas de frutas ou legumes, que misturam a polpa de um ou mais tipos de fruta ou legumes, cozidas em calda de açúcar, até atingirem uma consistência gelificada.',
+    preco: 'A partir de R$ 2,99',
+  },
+  {
+    id: 4,
+    name: 'Salgados',
+    image: 'img/salgado.jpg',
+    descri: 'Salgado ou salgadinho é o nome que se dá a aperitivos que podem ser pastelinhos fritos ou assados no forno, de carne ou queijo, além de queijos e embutidos em pedaços ou fatiados.',
+    preco: 'A partir de R$ 2,99',
+  },
+];
+
+export default dataset;
